f44a236772283ab4ae23337174c0b4971fd9f73a
Neil Martins <neil.martins@kilowott.com>
2025-09-20 18:59:23 +0530

    Minor UI Changes for FE(Show Hamburger menu)

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace MusicHFE2
             builder.Services.AddScoped<StateService>();
             builder.Services.AddScoped<AuthService>();
             builder.Services.AddScoped<JwtAuthStateProvider>();
+            builder.Services.AddScoped<IRazorPayService, RazorPayService>();
             builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<JwtAuthStateProvider>());
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped(sp =>
@@ -29,7 +30,7 @@ namespace MusicHFE2
     return client;
 });
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<IRazorPayService, RazorPayService>();
+            
             await builder.Build().RunAsync();
         }
     }
